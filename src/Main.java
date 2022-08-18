74182b3f5c7fcf42ac57e2ad1b2a2a2d63b91ba4
ZhdanovO <oleg.zhdanov88@gmail.com>
2022-08-18 07:37:03 +0300

    Just speed test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("Привет");
+        strings.add("меня");
+        strings.add("зовут");
+        strings.add("массив");
+
+        long startTime1 = System.currentTimeMillis();
+
+        for (int i = 0; i < 1000000; i++) {
+            strings.add(i, "не");
+        }
+
+        long finishTime1 = System.currentTimeMillis();
+        long duration =  finishTime1 - startTime1;
+
+//        for (String str : strings) {
+//            System.out.println(str);
+//        }
+
+        System.out.println("Время выполнения добавления миллиона элементов в ArrayList = " + duration + " миллисекунд");
+
+        LinkedList<String> linkedStrings = new LinkedList<>();
+        linkedStrings.add("Привет");
+        linkedStrings.add("меня");
+        linkedStrings.add("зовут");
+        linkedStrings.add("массив");
+
+        long startTime2 = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            linkedStrings.add(i, "не");
+        }
+        long finishTime2 = System.currentTimeMillis();
+        long duration2 =  finishTime2 - startTime2;
+        System.out.println("Время выполнения добавления миллиона элементов в LinkedList = " + duration2 + " миллисекунд");
+
+//        for (String str : linkedStrings) {
+//            System.out.println(str);
+//        }
 
     }
 }
